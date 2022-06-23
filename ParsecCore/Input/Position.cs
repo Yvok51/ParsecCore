cec7d9996ef2d888b4811645725ca61c8745cc90
tichymi5 <michal.tichy.y@email.cz>
2022-06-23 12:54:26 +0200

    Added StringParserInput

--- ParsecCore/Input/Position.cs
+++ ParsecCore/Input/Position.cs
@@ -2,8 +2,28 @@
 {
     struct Position
     {
+        public static Position Start = new Position(line: 1, column: 1, offset: 0);
+        
+        private Position(int line, int column, int offset)
+        {
+            Line = line;
+            Column = column;
+            Offset = offset;
+        }
+
         public int Line { get; init; }
         public int Column { get; init; }
+        public int Offset { get; init; }
+
+        public Position NextColumn()
+        {
+            return new Position(line: Line, column: Column + 1, offset: Offset + 1);
+        }
+
+        public Position NextLine()
+        {
+            return new Position(line: Line + 1, column: 1, offset: Offset + 1);
+        }
 
         public override string ToString()
         {
---
+++ ParsecCore/Input/StringParserInput.cs
@@ -0,0 +1,30 @@
+﻿namespace ParsecCore.Input
+{
+    class StringParserInput : IParserInput
+    {
+        public StringParserInput(string input)
+        {
+            _input = input;
+            _position = Position.Start;
+        }
+
+        public bool EndOfInput => _position.Offset >= _input.Length;
+
+        public Position Position => _position;
+
+        public char Read()
+        {
+            char readChar = _input[_position.Offset];
+            _position = readChar == '\n' ? _position.NextLine() : _position.NextColumn();
+            return readChar;
+        }
+
+        public void Seek(Position position)
+        {
+            _position = position;
+        }
+
+        private readonly string _input;
+        private Position _position;
+    }
+}
