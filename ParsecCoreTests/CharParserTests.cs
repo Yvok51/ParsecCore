95415a5dd19d8db744c4920f577e4f4402d7b255
tichymi5 <michal.tichy.y@email.cz>
2022-06-24 20:41:42 +0200

    Added Tests

---
+++ ParsecCoreTests/CharParserTests.cs
@@ -0,0 +1,74 @@
+using System;
+using Xunit;
+
+using ParsecCore;
+using ParsecCore.Input;
+using ParsecCore.EitherNS;
+
+namespace ParsecCoreTests
+{
+    public class CharParserTests
+    {
+        [Theory]
+        [InlineData("a", 'a')]
+        [InlineData("g", 'g')]
+        [InlineData("7", '7')]
+        public void ParseSingleLetter(string inputString, char toParse)
+        {
+            IParser<char> parser = Parser.Char(toParse);
+            IParserInput input = ParserInput.Create(inputString);
+
+            IEither<ParseError, char> result = parser.Parse(input);
+
+            Assert.True(result.HasRight);
+            Assert.Equal(toParse, result.Right);
+        }
+
+        [Theory]
+        [InlineData("abcd", 'a')]
+        [InlineData("ghjk", 'g')]
+        [InlineData("71025", '7')]
+        public void ParseMltipleLetters(string inputString, char toParse)
+        {
+            IParser<char> parser = Parser.Char(toParse);
+            IParserInput input = ParserInput.Create(inputString);
+
+            IEither<ParseError, char> result = parser.Parse(input);
+
+            Assert.True(result.HasRight);
+            Assert.Equal(toParse, result.Right);
+        }
+
+        [Theory]
+        [InlineData("abcd", 'a')]
+        [InlineData("ghjk", 'g')]
+        [InlineData("71025", '7')]
+        public void CorrectInputSizeAfterParse(string inputString, char toParse)
+        {
+            IParser<char> parser = Parser.Char(toParse);
+            IParserInput input = ParserInput.Create(inputString);
+
+            IEither<ParseError, char> result = parser.Parse(input);
+
+            Assert.True(result.HasRight);
+            Assert.Equal(toParse, result.Right);
+
+            Assert.False(input.EndOfInput);
+            Assert.Equal(1, input.Position.Offset);
+        }
+
+        [Theory]
+        [InlineData("abcd", 'n')]
+        [InlineData("ghjk", 'y')]
+        [InlineData("71025", 'c')]
+        public void ParseCorrectlyFails(string inputString, char toParse)
+        {
+            IParser<char> parser = Parser.Char(toParse);
+            IParserInput input = ParserInput.Create(inputString);
+
+            IEither<ParseError, char> result = parser.Parse(input);
+
+            Assert.True(result.HasLeft);
+        }
+    }
+}
---
+++ ParsecCoreTests/StringParserTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+using ParsecCore;
+using ParsecCore.Input;
+using ParsecCore.EitherNS;
+
+namespace ParsecCoreTests
+{
+    public class StringParserTests
+    {
+        [Theory]
+        [InlineData("a", "a")]
+        [InlineData("g", "g")]
+        [InlineData("7", "7")]
+        public void ParseSingleLetter(string inputString, string toParse)
+        {
+            IParser<string> parser = Parser.String(toParse);
+            IParserInput input = ParserInput.Create(inputString);
+
+            IEither<ParseError, string> result = parser.Parse(input);
+
+            Assert.True(result.HasRight);
+            Assert.Equal(toParse, result.Right);
+        }
+
+        [Theory]
+        [InlineData("abcd", "ab")]
+        [InlineData("ghjk", "ghjk")]
+        [InlineData("71025", "710")]
+        public void ParseMltipleLetters(string inputString, string toParse)
+        {
+            IParser<string> parser = Parser.String(toParse);
+            IParserInput input = ParserInput.Create(inputString);
+
+            IEither<ParseError, string> result = parser.Parse(input);
+
+            Assert.True(result.HasRight);
+            Assert.Equal(toParse, result.Right);
+        }
+
+        [Theory]
+        [InlineData("abcd", "ab")]
+        [InlineData("ghjk", "g")]
+        [InlineData("71025", "710")]
+        public void CorrectInputSizeAfterParse(string inputString, string toParse)
+        {
+            IParser<string> parser = Parser.String(toParse);
+            IParserInput input = ParserInput.Create(inputString);
+
+            IEither<ParseError, string> result = parser.Parse(input);
+
+            Assert.True(result.HasRight);
+            Assert.Equal(toParse, result.Right);
+
+            Assert.False(input.EndOfInput);
+            Assert.Equal(toParse.Length, input.Position.Offset);
+        }
+
+        [Theory]
+        [InlineData("abcd", "abcd")]
+        [InlineData("ghjk\nasf", "ghjk\nasf")]
+        [InlineData("71025", "71025")]
+        public void InputCorrectlySpentAfterParse(string inputString, string toParse)
+        {
+            IParser<string> parser = Parser.String(toParse);
+            IParserInput input = ParserInput.Create(inputString);
+
+            IEither<ParseError, string> result = parser.Parse(input);
+
+            Assert.True(result.HasRight);
+            Assert.Equal(toParse, result.Right);
+
+            Assert.True(input.EndOfInput);
+        }
+
+        [Theory]
+        [InlineData("abcd", "hello")]
+        [InlineData("ghjk\nasf", "there")]
+        [InlineData("71025", "general")]
+        public void ParseCorrectlyFailed(string inputString, string toParse)
+        {
+            IParser<string> parser = Parser.String(toParse);
+            IParserInput input = ParserInput.Create(inputString);
+
+            IEither<ParseError, string> result = parser.Parse(input);
+
+            Assert.True(result.HasLeft);
+        }
+
+        [Fact]
+        public void ParsingFailureLocationCorrect()
+        {
+            IParser<string> parser = Parser.String("abd");
+            IParserInput input = ParserInput.Create("abcd");
+
+            IEither<ParseError, string> result = parser.Parse(input);
+
+            Assert.True(result.HasLeft);
+            Assert.Equal(2, result.Left.Position.Offset);
+        }
+    }
+}
