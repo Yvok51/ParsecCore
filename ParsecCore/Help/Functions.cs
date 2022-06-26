47d99c21a973223b71a228646074f069ca2b6930
tichymi5 <michal.tichy.y@email.cz>
2022-06-26 21:14:11 +0200

    Added helpful function to pretty print enumerables

---
+++ ParsecCore/Help/Functions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ParsecCore.Help
+{
+    static class Functions
+    {
+        public static string ToPrettyString<T>(this IEnumerable<T> collection, string sep = ",")
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("[");
+            foreach (var item in collection)
+            {
+                builder.Append(" ");
+                builder.Append(item);
+                builder.Append(sep);
+            }
+            builder.Remove(builder.Length - sep.Length, sep.Length);
+            builder.Append("]");
+
+            return builder.ToString();
+        }
+    }
+}
