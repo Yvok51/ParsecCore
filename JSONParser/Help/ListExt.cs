2766f05936adbb1ab4fe64d0407c103821143d47
tichymi5 <michal.tichy.y@email.cz>
2022-06-30 11:41:06 +0200

    Added help function

---
+++ JSONParser/Help/ListExt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONtoXML
+{
+    static class Help
+    {
+        public static List<T> ListOf<T>(params T[] ts) => new List<T>(ts);
+    }
+}
