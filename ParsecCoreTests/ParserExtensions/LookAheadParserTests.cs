3f099edadf1137a18810985cb4bef08d6995c6da
tichymi5 <michal.tichy.y@email.cz>
2022-07-04 14:28:42 +0200

    Test reorganization

---
+++ ParsecCoreTests/ParserExtensions/LookAheadParserTests.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+
+using ParsecCore;
+using ParsecCore.Input;
+
+namespace ParsecCoreTests
+{
+    public class LookAheadParserTests
+    {
+        [Fact]
+        public void ParsedAndNothingConsumed()
+        {
+            var input = ParserInput.Create("12abc");
+            var parser = Parsers.String("12").LookAhead();
+            var initialPosition = input.Position;
+
+            var result = parser(input);
+
+            Assert.True(result.HasRight);
+            Assert.Equal("12", result.Right);
+            Assert.Equal(initialPosition, input.Position);
+        }
+
+        [Fact]
+        public void FailWithNothingConsumed()
+        {
+            var input = ParserInput.Create("abc");
+            var parser = Parsers.String("12").LookAhead();
+            var initialPosition = input.Position;
+
+            var result = parser(input);
+
+            Assert.True(result.HasLeft);
+            Assert.Equal(initialPosition, input.Position);
+        }
+
+        [Fact]
+        public void FailWithInputConsumed()
+        {
+            var input = ParserInput.Create("1abc");
+            var parser = Parsers.String("12").LookAhead();
+            var initialPosition = input.Position;
+
+            var result = parser(input);
+
+            Assert.True(result.HasLeft);
+            Assert.NotEqual(initialPosition, input.Position);
+        }
+    }
+}
