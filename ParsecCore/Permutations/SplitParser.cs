63babec5c0508825fcaab5f1eb052ad1e3586323
tichymi5 <michal.tichy.y@email.cz>
2022-11-16 17:35:19 +0100

    Change accessibilty modifiers of SplitParser so that its methods are not accessible by the end user

--- ParsecCore/Permutations/SplitParser.cs
+++ ParsecCore/Permutations/SplitParser.cs
@@ -4,17 +4,17 @@ namespace ParsecCore.Permutations
 {
     public class SplitParser<T, TInput>
     {
-        public SplitParser(Parser<T, TInput> parser, IMaybe<T> defaultValue)
+        internal SplitParser(Parser<T, TInput> parser, IMaybe<T> defaultValue)
         {
             _parser = parser;
             _defaultValue = defaultValue;
         }
 
-        public bool IsOptional { get => !_defaultValue.IsEmpty; }
+        internal bool IsOptional { get => !_defaultValue.IsEmpty; }
 
-        public Parser<T, TInput> Parser { get => _parser; }
+        internal Parser<T, TInput> Parser { get => _parser; }
 
-        public T DefaultValue { get => _defaultValue.Value; }
+        internal T DefaultValue { get => _defaultValue.Value; }
 
         private readonly Parser<T, TInput> _parser;
         private readonly IMaybe<T> _defaultValue;
