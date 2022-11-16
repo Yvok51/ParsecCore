3585ae58941e2ce6dc938be240161bbf9eb4d6d2
tichymi5 <michal.tichy.y@email.cz>
2022-11-16 17:31:24 +0100

    Added tests for Permutations

--- ParsecCoreTests/Permutations/PermutationsTests.cs
+++ ParsecCoreTests/Permutations/PermutationsTests.cs
@@ -1,6 +1,7 @@
 ﻿using ParsecCore;
 using ParsecCore.EitherNS;
 using ParsecCore.Input;
+using ParsecCore.Permutations;
 using Xunit;
 
 namespace ParsecCoreTests.Permutations
@@ -8,18 +9,102 @@ namespace ParsecCoreTests.Permutations
     public class PermutationsTests
     {
         [Theory]
-        [InlineData("a", "a")]
-        [InlineData("g", "g")]
-        [InlineData("7", "7")]
-        public void ParsingSingleMember(string inputString, string toParse)
+        [InlineData("a", 'a')]
+        [InlineData("g", 'g')]
+        [InlineData("7", '7')]
+        public void ParsingSingleMember(string inputString, char toParse)
         {
-            var parser = Parsers.String(toParse);
+            var parser = Permutation.Permute(Permutation.Parser(Parsers.Char(toParse)), (c) => c);
             var input = ParserInput.Create(inputString);
 
-            IEither<ParseError, string> result = parser(input);
+            IEither<ParseError, char> result = parser(input);
 
             Assert.True(result.IsResult);
             Assert.Equal(toParse, result.Result);
+        }
+
+        [Theory]
+        [InlineData("abcdefg", 'a')]
+        [InlineData("good morning, Vietnam!", 'g')]
+        [InlineData("7 sins", '7')]
+        public void ParsingSingleMemberFromLongerInput(string inputString, char toParse)
+        {
+            var parser = Permutation.Permute(Permutation.Parser(Parsers.Char(toParse)), (c) => c);
+            var input = ParserInput.Create(inputString);
+
+            IEither<ParseError, char> result = parser(input);
+
+            Assert.True(result.IsResult);
+            Assert.Equal(toParse, result.Result);
+        }
+
+        [Theory]
+        [InlineData("a", 'a', 'b')]
+        [InlineData("g", 'g', 'h')]
+        [InlineData("7", '7', '8')]
+        public void FinalTransformationFunctionIsAppliedOnSingleMember(string inputString, char toParse, char transformed)
+        {
+            var parser = Permutation.Permute(Permutation.Parser(Parsers.Char(toParse)), (c) => (char)(c + 1));
+            var input = ParserInput.Create(inputString);
+
+            IEither<ParseError, char> result = parser(input);
+
+            Assert.True(result.IsResult);
+            Assert.Equal(transformed, result.Result);
+        }
+
+        [Theory]
+        [InlineData("abc")]
+        [InlineData("acb")]
+        [InlineData("bac")]
+        [InlineData("bca")]
+        [InlineData("cab")]
+        [InlineData("cba")]
+        public void ParsingThreeMembers(string inputString)
+        {
+            var parser = Permutation.Permute(
+                Permutation.Parser(Parsers.Char('a')),
+                Permutation.Parser(Parsers.Char('b')),
+                Permutation.Parser(Parsers.Char('c')),
+                (a, b, c) => (a, b, c));
+            var input = ParserInput.Create(inputString);
+
+            IEither<ParseError, (char, char, char)> result = parser(input);
+
+            Assert.True(result.IsResult);
+            Assert.Equal(('a', 'b', 'c'), result.Result);
+        }
+
+        [Theory]
+        [InlineData("abc")]
+        [InlineData("acb")]
+        [InlineData("bac")]
+        [InlineData("bca")]
+        [InlineData("cab")]
+        [InlineData("cba")]
+        [InlineData("ca")]
+        [InlineData("ac")]
+        public void ParsingThreeMembersWithOptional(string inputString)
+        {
+            bool optionalNotInInput = inputString.Length < 3;
+            var parser = Permutation.Permute(
+                Permutation.Parser(Parsers.Char('a')),
+                Permutation.OptionalParser(Parsers.Char('b'), 'd'),
+                Permutation.Parser(Parsers.Char('c')),
+                (a, b, c) => (a, b, c));
+            var input = ParserInput.Create(inputString);
+
+            IEither<ParseError, (char, char, char)> result = parser(input);
+
+            Assert.True(result.IsResult);
+            if (optionalNotInInput)
+            {
+                Assert.Equal(('a', 'd', 'c'), result.Result);
+            }
+            else
+            {
+                Assert.Equal(('a', 'b', 'c'), result.Result);
+            }
         }
     }
 }
