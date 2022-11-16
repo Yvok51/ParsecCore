f1c6c3bde9f929d0bf41ce1bc45fa84a0e468f8c
tichymi5 <michal.tichy.y@email.cz>
2022-11-16 15:51:03 +0100

    Change Permute to static function

--- ParsecCore/Permutations/Perms.cs
+++ ParsecCore/Permutations/Perms.cs
@@ -6,123 +6,89 @@ using System.Threading.Tasks;
 
 namespace ParsecCore.Permutations
 {
-    class Perms<TA, TParserInput, TR>
+    internal static partial class Perms
     {
-        public Perms(SplitParser<TA, TParserInput> splitParser, Func<TA, TR> f)
+        public static Parser<TR, TParserInput> Permute<TA, TParserInput, TR>(
+            SplitParser<TA, TParserInput> splitParser,
+            Func<TA, TR> f
+        )
         {
-            _splitParser = splitParser;
-            _f = f;
-        }
-
-        public Parser<TR, TParserInput> Permute()
-        {
-            int branchCount = _splitParser.IsOptional ? 2 : 1;
+            int branchCount = splitParser.IsOptional ? 2 : 1;
             List<Parser<TR, TParserInput>> branches = new(branchCount);
 
             branches.Add(
-                from a in _splitParser.Parser
-                select _f(a)
+                from a in splitParser.Parser
+                select f(a)
             );
 
-            if (_splitParser.IsOptional)
+            if (splitParser.IsOptional)
             {
-                branches.Add(Parsers.Return<TR, TParserInput>(_f(_splitParser.DefaultValue)));
+                branches.Add(Parsers.Return<TR, TParserInput>(f(splitParser.DefaultValue)));
             }
 
             return Combinators.Choice(branches);
         }
 
-        private readonly SplitParser<TA, TParserInput> _splitParser;
-        private readonly Func<TA, TR> _f;
-    }
-
-    class Perms<TA, TB, TParserInput, TR>
-    {
-        public Perms(
+        public static Parser<TR, TParserInput> Permute<TA, TB, TParserInput, TR>(
             SplitParser<TA, TParserInput> splitParserA,
             SplitParser<TB, TParserInput> splitParserB,
-            Func<TA, TB, TR> f)
+            Func<TA, TB, TR> f
+        )
         {
-            _splitParserA = splitParserA;
-            _splitParserB = splitParserB;
-            _f = f;
-        }
-
-        public Parser<TR, TParserInput> Permute()
-        {
-            int branchCount = _splitParserA.IsOptional && _splitParserB.IsOptional ? 3 : 2;
+            int branchCount = splitParserA.IsOptional && splitParserB.IsOptional ? 3 : 2;
             List<Parser<TR, TParserInput>> branches = new(branchCount);
 
             branches.Add(
-                from a in _splitParserA.Parser
-                from r in (new Perms<TB, TParserInput, TR>(_splitParserB, _f.Partial(a))).Permute()
+                from a in splitParserA.Parser
+                from r in Permute(splitParserB, f.Partial(a))
                 select r
             );
             branches.Add(
-                from b in _splitParserB.Parser
-                from r in (new Perms<TA, TParserInput, TR>(_splitParserA, _f.Partial(b))).Permute()
+                from b in splitParserB.Parser
+                from r in Permute(splitParserA, f.Partial(b))
                 select r
             );
 
-            if (_splitParserA.IsOptional && _splitParserB.IsOptional)
+            if (splitParserA.IsOptional && splitParserB.IsOptional)
             {
-                branches.Add(Parsers.Return<TR, TParserInput>(_f(_splitParserA.DefaultValue, _splitParserB.DefaultValue)));
+                branches.Add(Parsers.Return<TR, TParserInput>(f(splitParserA.DefaultValue, splitParserB.DefaultValue)));
             }
 
             return Combinators.Choice(branches);
         }
 
-        private readonly SplitParser<TA, TParserInput> _splitParserA;
-        private readonly SplitParser<TB, TParserInput> _splitParserB;
-        private readonly Func<TA, TB, TR> _f;
-    }
-
-    class Perms<TA, TB, TC, TParserInput, TR>
-    {
-        public Perms(
+        public static Parser<TR, TParserInput> Permute<TA, TB, TC, TParserInput, TR>(
             SplitParser<TA, TParserInput> splitParserA,
             SplitParser<TB, TParserInput> splitParserB,
             SplitParser<TC, TParserInput> splitParserC,
-            Func<TA, TB, TC, TR> f)
+            Func<TA, TB, TC, TR> f
+        )
         {
-            _splitParserA = splitParserA;
-            _splitParserB = splitParserB;
-            _splitParserC = splitParserC;
-            _f = f;
-        }
-
-        public Parser<TR, TParserInput> Permute()
-        {
-            int branchCount = _splitParserA.IsOptional && _splitParserB.IsOptional && _splitParserC.IsOptional ? 3 : 2;
+            int branchCount = splitParserA.IsOptional && splitParserB.IsOptional && splitParserC.IsOptional ? 3 : 2;
             List<Parser<TR, TParserInput>> branches = new(branchCount);
 
             branches.Add(
-                from a in _splitParserA.Parser
-                from r in (new Perms<TB, TC, TParserInput, TR>(_splitParserB, _splitParserC, _f.Partial(a))).Permute()
+                from a in splitParserA.Parser
+                from r in Permute(splitParserB, splitParserC, f.Partial(a))
                 select r
             );
             branches.Add(
-                from b in _splitParserB.Parser
-                from r in (new Perms<TA, TC, TParserInput, TR>(_splitParserA, _splitParserC, _f.Partial(b))).Permute()
+                from b in splitParserB.Parser
+                from r in Permute(splitParserA, splitParserC, f.Partial(b))
                 select r
             );
             branches.Add(
-                from c in _splitParserC.Parser
-                from r in (new Perms<TA, TB, TParserInput, TR>(_splitParserA, _splitParserB, _f.Partial(c))).Permute()
+                from c in splitParserC.Parser
+                from r in Permute(splitParserA, splitParserB, f.Partial(c))
                 select r
             );
 
-            if (_splitParserA.IsOptional && _splitParserB.IsOptional && _splitParserC.IsOptional)
+            if (splitParserA.IsOptional && splitParserB.IsOptional && splitParserC.IsOptional)
             {
-                branches.Add(Parsers.Return<TR, TParserInput>(_f(_splitParserA.DefaultValue, _splitParserB.DefaultValue, _splitParserC.DefaultValue)));
+                branches.Add(Parsers.Return<TR, TParserInput>(f(splitParserA.DefaultValue, splitParserB.DefaultValue, splitParserC.DefaultValue)));
             }
 
             return Combinators.Choice(branches);
         }
-
-        private readonly SplitParser<TA, TParserInput> _splitParserA;
-        private readonly SplitParser<TB, TParserInput> _splitParserB;
-        private readonly SplitParser<TC, TParserInput> _splitParserC;
-        private readonly Func<TA, TB, TC, TR> _f;
     }
 }
