6f8187bb13f283bf8c7b30477b14872d43b86655
tichymi5 <michal.tichy.y@email.cz>
2023-02-15 16:42:58 +0100

    Add identifier tests

---
+++ PythonParserTests/Parser/LiteralTests/IdentifierTests.cs
@@ -0,0 +1,37 @@
+﻿
+using ParsecCore.Input;
+using PythonParser.Parser;
+
+namespace PythonParserTests.Parser.LiteralTests
+{
+    public class IdentifierTests
+    {
+        [Theory]
+        [InlineData("a", "a")]
+        [InlineData("abc", "abc")]
+        [InlineData("ab0c", "ab0c")]
+        [InlineData("_a", "_a")]
+        [InlineData("a_b_c_", "a_b_c_")]
+        public void ParsedCorrectly(string inputString, string expected)
+        {
+            var input = ParserInput.Create(inputString);
+            var result = Literals.Identifier(input);
+
+            Assert.True(result.IsResult);
+            Assert.Equal(expected, result.Result.Name);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(".name")]
+        [InlineData("-name")]
+        [InlineData("0name")]
+        public void ParsingFails(string inputString)
+        {
+            var input = ParserInput.Create(inputString);
+            var result = Literals.Identifier(input);
+
+            Assert.True(result.IsError);
+        }
+    }
+}
