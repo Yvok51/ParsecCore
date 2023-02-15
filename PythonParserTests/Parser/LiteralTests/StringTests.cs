d1ad9462e724991d350320953a50bbbe567bc32f
tichymi5 <michal.tichy.y@email.cz>
2023-02-15 15:59:48 +0100

    Add string tests

--- PythonParserTests/Parser/LiteralTests/StringTests.cs
+++ PythonParserTests/Parser/LiteralTests/StringTests.cs
@@ -1,7 +1,34 @@
 ﻿
+using ParsecCore.Input;
+using PythonParser.Parser;
+
 namespace PythonParserTests.Parser.LiteralTests
 {
-    internal class StringTests
+    public class StringTests
     {
+        [Theory]
+        [InlineData("\"\"", "")]
+        [InlineData("\"a\"", "a")]
+        [InlineData("\"Hello World\"", "Hello World")]
+        [InlineData("\"Hello\\nWorld\"", "Hello\nWorld")]
+        public void ParsedCorrectly(string inputString, string expected)
+        {
+            var input = ParserInput.Create(inputString);
+            var result = Literals.String(input);
+
+            Assert.True(result.IsResult);
+            Assert.Equal(expected, result.Result.Value);
+        }
+
+        [Theory]
+        [InlineData("\"Unended string")]
+        [InlineData("\"wrong escape \\ \"")]
+        public void ParsingFails(string inputString)
+        {
+            var input = ParserInput.Create(inputString);
+            var result = Literals.String(input);
+
+            Assert.True(result.IsError);
+        }
     }
 }
