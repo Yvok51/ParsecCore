54a0624a6e1c38abba9c7ddb9b098db0fc091c78
tichymi5 <michal.tichy.y@email.cz>
2023-02-19 23:09:28 +0100

    Added more Expression tests

---
+++ PythonParserTests/Parser/ExpressionTests/PowerTests.cs
@@ -0,0 +1,52 @@
+﻿
+using ParsecCore.Input;
+using PythonParser.Parser;
+using PythonParser.Structures;
+
+namespace PythonParserTests.Parser.ExpressionTests
+{
+    public class PowerTests
+    {
+        [Theory]
+        [InlineData("1**2", 1, 2)]
+        [InlineData("4 ** 20", 4, 20)]
+        public void PowerParsed(string inputString, int basis, int exponent)
+        {
+            var input = ParserInput.Create(inputString);
+            var result = Expressions.Expression(Control.Lexeme)(input);
+
+            Assert.True(result.IsResult);
+            Assert.True(result.Result is Binary);
+            Assert.Equal(
+                new Binary(new IntegerLiteral(basis), BinaryOperator.DoubleStar, new IntegerLiteral(exponent)),
+                result.Result
+            );
+        }
+
+        [Fact]
+        public void MultiplePowers()
+        {
+            var input = ParserInput.Create("1 ** 2 ** 3 ** 4");
+            var result = Expressions.Expression(Control.Lexeme)(input);
+
+            Assert.True(result.IsResult);
+            Assert.True(result.Result is Binary);
+            Assert.Equal(
+                new Binary(
+                    new IntegerLiteral(1),
+                    BinaryOperator.DoubleStar,
+                    new Binary(
+                        new IntegerLiteral(2),
+                        BinaryOperator.DoubleStar,
+                        new Binary(
+                            new IntegerLiteral(3),
+                            BinaryOperator.DoubleStar,
+                            new IntegerLiteral(4)
+                        )
+                    )
+                ),
+                result.Result
+            );
+        }
+    }
+}
---
+++ PythonParserTests/Parser/ExpressionTests/UnaryTests.cs
@@ -0,0 +1,48 @@
+﻿
+using ParsecCore.Input;
+using PythonParser.Parser;
+using PythonParser.Structures;
+
+namespace PythonParserTests.Parser.ExpressionTests
+{
+    public class UnaryTests
+    {
+        [Theory]
+        [InlineData("-2", UnaryOperator.Minus, 2)]
+        [InlineData("- 10", UnaryOperator.Minus, 10)]
+        [InlineData("+ 10", UnaryOperator.Plus, 10)]
+        internal void Unary(string inputString, UnaryOperator op, int num)
+        {
+            var input = ParserInput.Create(inputString);
+            var result = Expressions.Expression(Control.Lexeme)(input);
+
+            Assert.True(result.IsResult);
+            Assert.True(result.Result is Unary);
+            Assert.Equal(
+                new Unary(new IntegerLiteral(num), op),
+                result.Result
+            );
+        }
+
+        [Fact]
+        internal void ComplexUnary()
+        {
+            var input = ParserInput.Create("-2 ** -3");
+            var result = Expressions.Expression(Control.Lexeme)(input);
+
+            Assert.True(result.IsResult);
+            Assert.True(result.Result is Unary);
+            Assert.Equal(
+                new Unary(
+                    new Binary(
+                        new IntegerLiteral(2),
+                        BinaryOperator.DoubleStar,
+                        new Unary(new IntegerLiteral(3), UnaryOperator.Minus)
+                    ),
+                    UnaryOperator.Minus
+                ),
+                result.Result
+            );
+        }
+    }
+}
