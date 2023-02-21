9488ca6da5c963a2eda2a11421218ece4dbc8a23
tichymi5 <michal.tichy.y@email.cz>
2023-02-21 13:18:02 +0100

    Added top level parser

---
+++ PythonParser/Parser/TopLevelParser.cs
@@ -0,0 +1,15 @@
+﻿using ParsecCore;
+using PythonParser.Parser;
+using PythonParser.Structures;
+
+namespace PythonParser.TopLevelParser
+{
+    internal static class TopLevelParser
+    {
+        public static Parser<IReadOnlyList<Stmt>, char> PythonFile =
+            from leadingWhitespace in Control.EOLWhitespace
+            from stmts in Control.EOLLexeme.Create(Statements.Statement).Many()
+            from _ in Parsers.EOF
+            select stmts;
+    }
+}
