88ce838251c979dbbbd13fbaf97c802dc6b7e63f
tichymi5 <michal.tichy.y@email.cz>
2023-02-22 12:22:55 +0100

    Setup benchmark project

---
+++ JSONtoXML.Benchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace JSONtoXML.Benchmarks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = JSONtoXML.JSONParsers.JsonDocument;
+        }
+    }
+}
