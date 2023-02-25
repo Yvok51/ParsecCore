14ed5ebee41ef2e0598afab072158e1ba46b81fc
tichymi5 <michal.tichy.y@email.cz>
2023-02-25 23:11:15 +0100

    Change the printing of XML to use the new visitor pattern

--- JSONParser/PrintXML.cs
+++ JSONParser/PrintXML.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using JSONtoXML.XMLValues;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace JSONtoXML
@@ -10,93 +12,52 @@ namespace JSONtoXML
         {
             var xmlDeclaration = $"<?xml version=\"1.0\" encoding=\"{writer.Encoding.WebName}\"?>";
             writer.Write(xmlDeclaration);
-            PrintImpl(node, writer, 0);
-            writer.WriteLine();
+            writer.WriteLine(node.Accept(new XMLtoString(), 0));
             writer.Flush();
         }
 
-        private static void PrintImpl(XMLNode node, TextWriter writer, int indentation)
+        private class XMLtoString : IXMLVisitor<string, int>
         {
-            if (node is XMLTextNode textNode)
+            public XMLtoString(int spacesPerIndent=4)
             {
-                PrintImpl(textNode, writer, indentation);
-            }
-            else if (node is XMLElementNode elNode)
-            {
-                PrintImpl(elNode, writer, indentation);
-            }
-        }
-
-        private static void PrintImpl(XMLElementNode node, TextWriter writer, int indentation)
-        {
-            writer.WriteLine();
-            writer.Write(IndentationToString(indentation));
-            writer.Write($"<{node.Tag}{AttributesToString(node.Attributes)}");
-
-            if (node.ChildNodes.Count == 0)
-            {
-                writer.Write("/>");
-                return;
-            }
-            if (node.ChildNodes.Count == 1 && node.ChildNodes[0] is XMLTextNode textNode)
-            {
-                writer.Write(">");
-                PrintImpl(textNode, writer, 0);
-                writer.Write($"</{node.Tag}>");
-                return;
+                _spacesPerIndent = spacesPerIndent;
             }
 
-            writer.Write(">");
-
-            foreach (var child in node.ChildNodes)
+            public string ElementVisit(XMLElementNode element, int indent)
             {
-                PrintImpl(child, writer, indentation + 1);
-            }
-            writer.WriteLine();
-            writer.Write(IndentationToString(indentation));
-            writer.Write($"</{node.Tag}>");
-        }
-
-        private static void PrintImpl(XMLTextNode node, TextWriter writer, int indentation)
-        {
-            writer.Write(IndentationToString(indentation));
-            writer.Write(node.Content);
-        }
-
-        private static string singleIndentation = "  ";
-
-        private static string IndentationToString(int indentation)
-        {
-            StringBuilder builder = new StringBuilder(indentation * singleIndentation.Length);
-            for (int i = 0; i < indentation; i++)
-            {
-                builder.Append(singleIndentation);
-            }
-            return builder.ToString();
-        }
-
-        private static void WriteIndentation(TextWriter writer, int indentation)
-        {
-            for (int i = 0; i < indentation; i++)
-            {
-                writer.Write(indentation);
-            }
-        }
-
-        private static string AttributesToString(Dictionary<string, string> attributes)
-        {
-            if (attributes is null)
-            {
-                return string.Empty;
+                return GetIndentation(indent, _spacesPerIndent)
+                    + $"<{element.Tag}{AttributesToString(element.Attributes)}>\n"
+                    + string.Concat(element.ChildNodes.Select(node => element.Accept(this, indent + 1)))
+                    + $"</{element.Tag}>";
             }
 
-            StringBuilder builder = new StringBuilder();
-            foreach (var (key, value) in attributes)
+            public string TextVisit(XMLTextNode text, int indent)
             {
-                builder.Append($" {key}=\"{value}\"");
+                return GetIndentation(indent, _spacesPerIndent) + text.Content + '\n';
             }
 
-            return builder.ToString();
+            private static string GetIndentation(int indent, int spacesPerIndent)
+            {
+                return new string(' ', indent * spacesPerIndent);
+            }
+
+            private static string AttributesToString(Dictionary<string, string> attributes)
+            {
+                if (attributes is null)
+                {
+                    return string.Empty;
+                }
+
+                StringBuilder builder = new StringBuilder();
+                foreach (var (key, value) in attributes)
+                {
+                    builder.Append($" {key}=\"{value}\"");
+                }
+
+                return builder.ToString();
+            }
+
+            private readonly int _spacesPerIndent;
         }
     }
 }
