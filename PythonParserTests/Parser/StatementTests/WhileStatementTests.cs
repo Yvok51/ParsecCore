453beaf7cf70be4eaafc36c49ba5a4e10240bac0
tichymi5 <michal.tichy.y@email.cz>
2023-02-26 12:07:46 +0100

    Add more python parser tests

--- PythonParserTests/Parser/StatementTests/WhileStatementTests.cs
+++ PythonParserTests/Parser/StatementTests/WhileStatementTests.cs
@@ -68,6 +68,46 @@ namespace PythonParserTests.Parser.StatementTests
             );
         }
 
+        [Theory]
+        [InlineData("while x:\n  call()\n\n  return\n")]
+        [InlineData("while x:\n\n  call()\n\n  return\n")]
+        [InlineData("while x:  \n\n \n  \n   \n  call()\n \n  \n   \n  return\n\n")]
+        public void EmptyLines(string inputString)
+        {
+            var input = ParserInput.Create(inputString);
+            var result = Statements.Statement(input);
+
+            Assert.True(result.IsResult);
+            Assert.Equal(
+                new While(
+                    new IdentifierLiteral("x"),
+                    new Suite(new List<Stmt>()
+                    {
+                        new Suite(
+                            new List<Stmt>()
+                            {
+                                new ExpressionStmt(
+                                    new List<Expr>()
+                                    {
+                                        new Call(
+                                            new IdentifierLiteral("call"),
+                                            Maybe.Nothing<IReadOnlyList<Expr>>(),
+                                            Maybe.Nothing<IReadOnlyList<KeywordArgument>>(),
+                                            Maybe.Nothing<Expr>(),
+                                            Maybe.Nothing<Expr>()
+                                        )
+                                    }
+                                )
+                            }
+                        ),
+                        new Suite( new List<Stmt>() { new Return(Maybe.Nothing<IReadOnlyList<Expr>>()) })
+                    }),
+                    Maybe.Nothing<Suite>()
+                ),
+                result.Result
+            );
+        }
+
         [Fact]
         public void LongerCondition()
         {
