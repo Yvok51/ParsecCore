8e105e445e19279c62a7fe1dd82c2cf1ed3256b5
tichymi5 <michal.tichy.y@email.cz>
2023-03-01 10:48:57 +0100

    Added benchmarks

--- JSONtoXML.Benchmarks/Program.cs
+++ JSONtoXML.Benchmarks/Program.cs
@@ -1,50 +1,136 @@
 ﻿
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
-using CommandLine;
 using System.Diagnostics;
-using ParsecCore;
 using ParsecCore.Input;
-using ParsecCore.EitherNS;
+using BenchmarkDotNet.Engines;
 
 namespace JSONtoXML.Benchmarks
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            // var parser = JSONtoXML.JSONParsers.JsonDocument;
+            var summaryMacro = BenchmarkRunner.Run<ParseJSONMacro>();
+            var summaryMicro = BenchmarkRunner.Run<ParseJSONMicro>();
 
-            IParserInput<char> source = null;
-
-            try
-            {
-                using (StreamReader reader = new StreamReader(args[0]))
-                {
-                    source = ParserInput.Create(reader.ReadToEnd());
-                }
-            }
-            catch
-            {
-            }
-
-            var summary = BenchmarkRunner.Run<ParseJSON>();
+            Console.WriteLine(summaryMicro);
+            Console.WriteLine(summaryMacro);
         }
 
-        public class ParseJSON
+        [SimpleJob(RunStrategy.Monitoring, launchCount: 30, warmupCount: 0, iterationCount: 30)]
+        public class ParseJSONMacro
         {
-            public ParseJSON(IParserInput<char> source, Parser<JsonValue, char> parser)
+            public ParseJSONMacro()
             {
-                _parser = parser;
-                _source = source;
+                Files = LoadFiles(FileNames());
             }
 
+            public IEnumerable<string> FileNames()
+            {
+                yield return "test-files/zips.json";
+                //yield return "test-files/large.json";
+            }
+
+            public Dictionary<string, string> Files;
+
             [Benchmark]
-            public IEither<ParseError, JsonValue> Parse() => _parser(_source);
+            [ArgumentsSource(nameof(FileNames))]
+            public bool ParseMacro(string fileName) => JSONParsers.JsonDocument(ParserInput.Create(Files[fileName])).IsResult;
 
-            private readonly Parser<JsonValue, char> _parser;
-            private readonly IParserInput<char> _source;
+            public static Dictionary<string, string> LoadFiles(IEnumerable<string> files)
+            {
+                var fileContent = new Dictionary<string, string>();
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        using (StreamReader reader = new StreamReader(file))
+                        {
+                            fileContent.Add(file, reader.ReadToEnd());
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
 
+                return fileContent;
+            }
+        }
+
+        public class ParseJSONMicro
+        {
+            [Benchmark]
+            [Arguments(@"
+{
+    ""glossary"": {
+        ""title"": ""example glossary"",
+        ""GlossDiv"": {
+            ""title"": ""S"",
+            ""GlossList"": {
+                ""GlossEntry"": {
+                    ""ID"": 1526,
+                    ""SortAs"": ""SGML"",
+                    ""GlossTerm"": ""Standard Generalized Markup Language"",
+                    ""Acronym"": null,
+                    ""Abbrev"": ""ISO 8879:1986"",
+                    ""GlossDef"": {
+                        ""para"": ""A meta-markup language, used to create markup languages such as DocBook."",
+                        ""GlossSeeAlso"": [""GML"", ""XML""]
+                    },
+                    ""GlossSee"": true
+                }
+            }
+        }
+    }
+}
+")]
+            [Arguments(@"
+{""widget"": {
+    ""debug"": ""on"",
+    ""window"": {
+        ""title"": ""Sample Konfabulator Widget"",
+        ""name"": ""main_window"",
+        ""width"": 500,
+        ""height"": 500
+    },
+    ""image"": { 
+        ""src"": ""Images/Sun.png"",
+        ""name"": ""sun1"",
+        ""hOffset"": 250,
+        ""vOffset"": 250,
+        ""alignment"": ""center""
+    },
+    ""text"": {
+        ""data"": ""Click Here"",
+        ""size"": 36,
+        ""style"": ""bold"",
+        ""name"": ""text1"",
+        ""hOffset"": 250,
+        ""vOffset"": 100,
+        ""alignment"": ""center"",
+        ""onMouseUp"": ""sun1.opacity = (sun1.opacity / 100) * 90;""
+    }
+}}")]
+            [Arguments(@"
+{""menu"": {
+    ""id"": ""file"",
+    ""value"": ""File"",
+    ""popup"": {
+        ""menuitem"": [
+            {""value"": ""New"", ""onclick"": ""CreateNewDoc()""},
+            {""value"": ""Open"", ""onclick"": ""OpenDoc()""},
+            {""value"": ""Close"", ""onclick"": ""CloseDoc()""}
+        ]
+    }
+}}
+")]
+            [Arguments("[\"Simple JSON list\", 1, 2, 3, 4, 5, \"a\", \"b\", \"c\", \"d\", \"e\"]")]
+            [Arguments("{\"key0\": \"Simple JSON object\", \"key1\": 1, \"key2\": 2, \"key3\": 3, " +
+                "\"key4\": \"a\", \"key5\": \"b\", \"key6\": \"c\"}")]
+            public bool ParseMicro(string input) => JSONParsers.JsonDocument(ParserInput.Create(input)).IsResult;
         }
     }
 }
---
+++ JSONtoXML.Benchmarks/README.md
@@ -0,0 +1,10 @@
+# ParsecCore JSON parser benchmarks
+
+Benchmarks for the JSON parser used in JSONtoXML project. Serves as benchmarks for ParsecCore library itself.
+The examples used for benchmarking were taken from [here](https://www.json.org/example.html).
+
+Recommended to be run from the command line with the following command:
+
+```sh
+dotnet build -c Release
+```
