99399dad01cf7e6ad74b3003d410ee5d06c6f9dd
tichymi5 <michal.tichy.y@email.cz>
2023-04-26 22:29:20 +0200

    Move benchmark data to files

--- benchmarks/JSONtoXML.Benchmarks/Program.cs
+++ benchmarks/JSONtoXML.Benchmarks/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Engines;
 using BenchmarkDotNet.Running;
 using ParsecCore;
@@ -8,105 +7,64 @@ namespace JSONtoXML.Benchmarks
 {
     public class Program
     {
+        public static readonly string FilePath = "test-files/";
+
         public static void Main(string[] args)
         {
             var summaryMicro = BenchmarkRunner.Run<ParseJSONMicro>();
-            var summaryMacro = BenchmarkRunner.Run<ParseJSONMacro>();
+            var summaryLarge = BenchmarkRunner.Run<ParseJSONMacro>();
+            var summaryEmpty = BenchmarkRunner.Run<ParseJSONTest>();
 
             Console.WriteLine(summaryMicro);
-            Console.WriteLine(summaryMacro);
+            Console.WriteLine(summaryLarge);
+            Console.WriteLine(summaryEmpty);
         }
 
         [SimpleJob(RunStrategy.Monitoring, launchCount: 5, warmupCount: 0, iterationCount: 20)]
         public class ParseJSONMacro
         {
-            public ParseJSONMacro()
-            { }
-
             public IEnumerable<string> FileNames()
             {
-                yield return "test-files/countries.json";
-                yield return "test-files/zips.json";
-                yield return "test-files/large.json";
+                yield return "zips.json";
+                yield return "large.json";
             }
 
             [Benchmark]
             [ArgumentsSource(nameof(FileNames))]
-            public bool ParseMacro(string fileName) => JSONParsers.JsonDocument(ParserInput.Create(File.ReadAllText(fileName))).IsResult;
+            public bool ParseMacro(string fileName)
+                => JSONParsers.JsonDocument(ParserInput.Create(File.ReadAllText(FilePath + fileName))).IsResult;
 
         }
 
         public class ParseJSONMicro
         {
-            [Benchmark]
-            [Arguments(@"
-{
-    ""glossary"": {
-        ""title"": ""example glossary"",
-        ""GlossDiv"": {
-            ""title"": ""S"",
-            ""GlossList"": {
-                ""GlossEntry"": {
-                    ""ID"": 1526,
-                    ""SortAs"": ""SGML"",
-                    ""GlossTerm"": ""Standard Generalized Markup Language"",
-                    ""Acronym"": null,
-                    ""Abbrev"": ""ISO 8879:1986"",
-                    ""GlossDef"": {
-                        ""para"": ""A meta-markup language, used to create markup languages such as DocBook."",
-                        ""GlossSeeAlso"": [""GML"", ""XML""]
-                    },
-                    ""GlossSee"": true
-                }
+            public IEnumerable<string> FileNames()
+            {
+                yield return "countries.json";
+                yield return "complex1.json";
+                yield return "complex2.json";
+                yield return "complex3.json";
+                yield return "list.json";
+                yield return "object.json";
             }
+
+            [Benchmark]
+            [ArgumentsSource(nameof(FileNames))]
+            public bool ParseMacro(string fileName)
+                => JSONParsers.JsonDocument(ParserInput.Create(File.ReadAllText(FilePath + fileName))).IsResult;
         }
-    }
-}
-")]
-            [Arguments(@"
-{""widget"": {
-    ""debug"": ""on"",
-    ""window"": {
-        ""title"": ""Sample Konfabulator Widget"",
-        ""name"": ""main_window"",
-        ""width"": 500,
-        ""height"": 500
-    },
-    ""image"": { 
-        ""src"": ""Images/Sun.png"",
-        ""name"": ""sun1"",
-        ""hOffset"": 250,
-        ""vOffset"": 250,
-        ""alignment"": ""center""
-    },
-    ""text"": {
-        ""data"": ""Click Here"",
-        ""size"": 36,
-        ""style"": ""bold"",
-        ""name"": ""text1"",
-        ""hOffset"": 250,
-        ""vOffset"": 100,
-        ""alignment"": ""center"",
-        ""onMouseUp"": ""sun1.opacity = (sun1.opacity / 100) * 90;""
-    }
-}}")]
-            [Arguments(@"
-{""menu"": {
-    ""id"": ""file"",
-    ""value"": ""File"",
-    ""popup"": {
-        ""menuitem"": [
-            {""value"": ""New"", ""onclick"": ""CreateNewDoc()""},
-            {""value"": ""Open"", ""onclick"": ""OpenDoc()""},
-            {""value"": ""Close"", ""onclick"": ""CloseDoc()""}
-        ]
-    }
-}}
-")]
-            [Arguments("[\"Simple JSON list\", 1, 2, 3, 4, 5, \"a\", \"b\", \"c\", \"d\", \"e\"]")]
-            [Arguments("{\"key0\": \"Simple JSON object\", \"key1\": 1, \"key2\": 2, \"key3\": 3, " +
-                "\"key4\": \"a\", \"key5\": \"b\", \"key6\": \"c\"}")]
-            public bool ParseMicro(string input) => JSONParsers.JsonDocument(ParserInput.Create(input)).IsResult;
+
+        public class ParseJSONTest
+        {
+            public IEnumerable<string> FileNames()
+            {
+                yield return "empty.json";
+            }
+
+            [Benchmark]
+            [ArgumentsSource(nameof(FileNames))]
+            public bool ParseMacro(string fileName)
+                => JSONParsers.JsonDocument(ParserInput.Create(File.ReadAllText(FilePath + fileName))).IsResult;
         }
     }
 }
