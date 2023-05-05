ce80347860bec018b6de868aabb72b3b3d4b377d
tichymi5 <michal.tichy.y@email.cz>
2023-05-05 13:40:40 +0200

    Added another example file

---
+++ examples/src/PythonParser/example-error.py
@@ -0,0 +1,13 @@
+vstup = []
+vstup = input().split()
+
+i = 0
+pocet = 0
+while '.' not in vstup[i]:
+    pocet = pocet + 1
+  i = i + 1
+
+if vstup[i] != '.':
+    pocet = pocet + 1
+
+print(pocet)
